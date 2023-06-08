8c4a623fe6383a67f1b546a54598a9887e8e3783
jeremymarian <marianojeremias.m@gmail.com>
2023-06-08 17:17:48 -0300

    visualmod

--- src/Pages/Initpage.jsx
+++ src/Pages/Initpage.jsx
@@ -20,28 +20,28 @@ const context = useContext(AuthContext)
      
      <>
      
-      <Navbar fixed="top" collapseOnSelect expand="lg"  bg="dark" variant="dark" >
-          <Container  fluid>
+      <Navbar className="navbarpr" collapseOnSelect expand="md"  bg="dark" variant="dark" >
+          <Container style={{display:"flex", justifyContent:"center"}}  fluid>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse   id="responsive-navbar-nav">
           {context.login && <>
-          <Nav className="me-auto" style={{display:"flex",justifyContent:"center"}}>
-          <Container style={{display:"flex",}} fluid>
+          <Nav className="me-auto" >
+          <Container style={{display:"flex", justifyContent:"center", marginRight:"50px"}} fluid>
           <Navbar.Brand href="#home">
             ¡Bienvenido! tu nombre es: {context.userName}
           </Navbar.Brand>
           </Container>
           </Nav>
-          <Nav className="ms-auto" style={{marginRight:"50px"}}>
+          <Nav className="ms-auto" >
           <Nav.Link as ={Link} to="/productos">Productos</Nav.Link>
-          <NavDropdown title="Mas Herramientas" id="collasible-nav-dropdown">
+          <NavDropdown style={{display:"flex", alignItems:"center" , flexDirection:"column"}} title="Mas Herramientas" id="collasible-nav-dropdown">
           <NavDropdown.Item as ={Link} to="/alta">Dar de alta un producto</NavDropdown.Item>
           <NavDropdown.Item as ={Button} onClick={context.handlerLogOut} >Salir</NavDropdown.Item>
           </NavDropdown></Nav></>}
           {
           !context.login && <>
           <Nav className="me-auto">
-          <Container style={{display:"flex",}} className="me-auto" fluid>
+          <Container style={{display:"flex", justifyContent:"center", marginLeft:"10px"}} className="me-auto" fluid>
           <Navbar.Brand href="#home">
            Usuario Desconocido
           </Navbar.Brand>
@@ -49,7 +49,7 @@ const context = useContext(AuthContext)
           </Nav>
           <Nav className="ms-auto">
           <Nav.Link as ={Link} to="/singup">Unete a Nosotros</Nav.Link>
-          <NavDropdown title="Mas Herramientas" id="collasible-nav-dropdown">
+          <NavDropdown style={{display:"flex", alignItems:"center" , flexDirection:"column"}} title="Mas Herramientas" id="collasible-nav-dropdown">
           <NavDropdown.Item as ={Link} to="/login">Log in</NavDropdown.Item> </NavDropdown>
           </Nav>
           </>
